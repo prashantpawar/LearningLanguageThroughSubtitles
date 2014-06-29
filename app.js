f3badbfc78460619d88069b9bf74d10a07fa9892
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-06-29 16:04:23 -0400

    - Fetches 20 frequent words - Fetches 20 records for those frequent words

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-var modules = {},
+var m = {},
     connection,
     db;
 
-modules.config = require('./config');
-modules.mysql = require('mysql');
-modules.util = require('util');
-modules.q = require('q');
-db = modules.config.db;
+m.config = require('./config');
+m.mysql = require('mysql');
+m.util = require('util');
+m.async = require('async');
+db = m.config.db;
 
-connection = modules.mysql.createConnection({
+connection = m.mysql.createConnection({
     host: db.host,
     user: db.user,
     password: db.password,
     database: db.database
 });
 
-var high_freq_query = "SELECT * FROM frequency_ru f WHERE f.id < 100 AND CHAR_LENGTH(f.word) > 3 ORDER BY RAND() LIMIT 25";
-var keyword_query = "SELECT * FROM translation t WHERE t.ru LIKE '%s' AND ABS(t.en_word_length - t.ru_word_length) = 0 ORDER BY t.ru_word_length ASC LIMIT 20";
 
+/**
 connection.query(high_freq_query, function(err, rows, fields) {
     if (err) throw err;
 
     rows.forEach(function(row) {
-        connection.query(modules.util.format(keyword_query, row.word), function (err, rows, field) {
+        connection.query(m.util.format(keyword_query, row.word), function (err, rows, field) {
             if (err) throw err;
 
             rows.forEach(function(row) {
@@ -32,5 +31,26 @@ connection.query(high_freq_query, function(err, rows, fields) {
     });
     connection.end();
 });
+**/
 
 //connection.end();
+
+m.async.waterfall([
+    function high_frequncy(callback) {
+        var high_freq_query = "SELECT * FROM frequency_ru f WHERE f.id < 100 AND CHAR_LENGTH(f.word) > 3 ORDER BY RAND() LIMIT 25";
+        connection.query(high_freq_query, function(err, rows, fields) {
+            callback(err, rows);
+        });
+    },
+    function fetch_translations(keywords, callback) {
+        var keyword_query = "SELECT * FROM translation t WHERE MATCH(t.ru) AGAINST('%s') AND ABS(t.en_word_length - t.ru_word_length) = 0 ORDER BY t.ru_word_length ASC LIMIT 20";
+        //console.log(m.util.format(keyword_query, keywords[0].word));
+        connection.query(m.util.format(keyword_query, keywords[0].word), function (err, rows, field) {
+            callback(err, rows);
+        });
+    }], 
+    function end_connection(err, result) {
+        console.log(result);
+        connection.end();
+    }
+);
